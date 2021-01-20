564c344d8f0921589a268e0ab5fe6d12fe6a2c4a
ritosonn <ritosonn@gmail.com>
2021-01-20 21:32:58 +0900

    merge model script

--- object_detection/fcos/model.py
+++ object_detection/fcos/model.py
@@ -1,4 +1,5 @@
 from typing import NamedTuple
+import timm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,6 +14,32 @@ class FCOSConfig(NamedTuple):
     x32_ch: int
     fpn_ch: int
     n_cls: int
+
+class EfficientNetBackbone(nn.Module):
+    def __init__(self, model_name: str):
+        super().__init__()
+        model = timm.create_model(model_name, pretrained=True)
+        self.backbone_8 = nn.Sequential(
+            model.conv_stem,
+            model.bn1,
+            model.act1,
+            model.blocks[0],
+            model.blocks[1],
+            model.blocks[2],
+        )
+        self.backbone_16 = nn.Sequential(
+            model.blocks[3],
+            model.blocks[4],
+        )
+        self.backbone_32 = nn.Sequential(
+            model.blocks[5],
+            model.blocks[6],
+        )
+    def forward(self, x):
+        out_8 = self.backbone_8(x)
+        out_16 = self.backbone_16(out_8)
+        out_32 = self.backbone_32(out_16)
+        return out_8, out_16, out_32
 
 class FeaturePyramid(nn.Module):
     def __init__(self, cfg: FCOSConfig):
